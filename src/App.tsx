12a8264d76474830d07a340d43d31a5fdc995d7b
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-01-28 18:34:31 -0300

    2.4

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={home} />
-            <IonLabel>Home</IonLabel>
+            <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
