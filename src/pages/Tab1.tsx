89da450275786ab2b23a264e33974cae3b52cdfe
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-01-28 16:32:13 -0300

    1.1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -8,16 +8,16 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>HOME</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">HOME</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="HOME page" />
       </IonContent>
     </IonPage>
   );
