3b12ecf19d1026f58fbc5d8d94a39d4bac75e238
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-01-28 17:09:26 -0300

    1.4

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonItem, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -19,15 +19,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         
-       <IonItem>
-          <IonLabel>Checkbox</IonLabel>
-          <IonCheckbox slot="start" />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>Range</IonLabel>
-          <IonRange></IonRange>
-        </IonItem>
+       <ExploreContainer name="HOME JOPO" />
         
       </IonContent>
     </IonPage>
